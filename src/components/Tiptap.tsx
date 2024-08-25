2edecddb2d64b59f3ca8e36f8fe922264be84d34
Dan Louis Dela Cruz <louisdc002@gmail.com>
2024-08-25 21:59:36 +0800

    Add more tiptap nodes like image, headings, and others

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -12,21 +12,30 @@ import {
   FormatListBulleted,
   FormatListNumbered,
   FormatQuote,
+  FormatStrikethrough,
   FormatUnderlined,
+  HorizontalRule,
   InsertPageBreakRounded,
+  InsertPhoto,
 } from "@mui/icons-material";
 import {
-  IconButton,
+  Box,
+  Button,
+  Modal,
   Paper,
+  TextField,
   ToggleButton,
   ToggleButtonGroup,
   Tooltip,
+  Typography,
 } from "@mui/material";
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, mergeAttributes } from "@tiptap/react";
 import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
+import Heading from "@tiptap/extension-heading";
+import Image from "@tiptap/extension-image";
 import StarterKit from "@tiptap/starter-kit";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
 const Tiptap = ({
   setContent,
@@ -34,6 +43,8 @@ const Tiptap = ({
   setContent: (value: React.SetStateAction<string>) => void;
 }) => {
   const [alignment, setAlignment] = React.useState("left");
+  const [openImageUrlInput, setOpenImageUrlInput] = useState(false);
+  const [imageURL, setImageUrl] = useState("");
 
   const handleChange = (event: any, newAlignment: any) => {
     setAlignment(newAlignment);
@@ -63,12 +74,32 @@ const Tiptap = ({
             class: "list-disc ms-8",
           },
         },
+        heading: false,
       }),
       Underline,
-
+      Image,
       TextAlign.configure({
         types: ["heading", "paragraph"],
       }),
+      Heading.extend({
+        levels: [1, 2],
+        renderHTML({ node, HTMLAttributes }) {
+          const level = this.options.levels.includes(node.attrs.level)
+            ? node.attrs.level
+            : this.options.levels[0];
+          const classes: { [index: number]: string } = {
+            1: "text-2xl",
+            2: "text-xl",
+          };
+          return [
+            `h${level}`,
+            mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+              class: `${classes[level]} font-semibold`,
+            }),
+            0,
+          ];
+        },
+      }).configure({ levels: [1, 2] }),
     ],
     immediatelyRender: false,
     content: "",
@@ -83,14 +114,103 @@ const Tiptap = ({
       setContent(json);
     },
   });
-
   if (!editor) {
     return null;
+  }
+  function addImage() {
+    setOpenImageUrlInput(true);
+  }
+  function closeImageModal() {
+    setOpenImageUrlInput(false);
+  }
+  function imageUrlSubmit() {
+    if (imageURL.trim() !== "" && editor !== null) {
+      editor.chain().focus().setImage({ src: imageURL }).run();
+    }
+
+    closeImageModal();
   }
 
   return (
     <Paper variant="outlined">
-      <div className="bg-gray-100 border-b px-2 py-1 flex items-center gap-4 justify-end">
+      <Modal
+        open={openImageUrlInput}
+        onClose={closeImageModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{ bgcolor: "background.paper" }}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 rounded-lg"
+        >
+          <Typography id="modal-modal-title" className="mb-3">
+            Insert Image URL:
+          </Typography>
+
+          <TextField
+            variant="outlined"
+            size="small"
+            placeholder="https://website.com/image.jpeg"
+            onChange={(e) => {
+              setImageUrl(e.target.value);
+            }}
+            className="mb-2"
+          />
+          <div className="flex justify-end">
+            <Button
+              variant="contained"
+              type="button"
+              onClick={imageUrlSubmit}
+              disableElevation
+            >
+              Insert Photo
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+      <div className="bg-gray-100 border-b px-2 py-1 flex items-center gap-4 justify-center">
+        <ToggleButtonGroup aria-label="text style" size="small">
+          <Tooltip title="H1">
+            <ToggleButton
+              value="h1"
+              aria-label="h1"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 1 }).run()
+              }
+              selected={editor.isActive("heading", { level: 1 }) ? true : false}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="13px"
+                viewBox="0 -960 960 960"
+                width="13px"
+                fill="inherit"
+              >
+                <path d="M200-280v-400h80v160h160v-160h80v400h-80v-160H280v160h-80Zm480 0v-320h-80v-80h160v400h-80Z" />
+              </svg>
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title="H2">
+            <ToggleButton
+              value="h2"
+              aria-label="h2"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 2 }).run()
+              }
+              selected={editor.isActive("heading", { level: 2 }) ? true : false}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="13px"
+                viewBox="0 -960 960 960"
+                width="13px"
+                fill="inherit"
+              >
+                <path d="M120-280v-400h80v160h160v-160h80v400h-80v-160H200v160h-80Zm400 0v-160q0-33 23.5-56.5T600-520h160v-80H520v-80h240q33 0 56.5 23.5T840-600v80q0 33-23.5 56.5T760-440H600v80h240v80H520Z" />
+              </svg>
+            </ToggleButton>
+          </Tooltip>
+        </ToggleButtonGroup>
         <ToggleButtonGroup aria-label="text formatting" size="small">
           <Tooltip title="Bold">
             <ToggleButton
@@ -99,7 +219,7 @@ const Tiptap = ({
               selected={editor.isActive("bold") ? true : false}
               onClick={() => editor.chain().focus().toggleBold().run()}
             >
-              <FormatBold />
+              <FormatBold fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           <Tooltip title="Italic">
@@ -109,7 +229,7 @@ const Tiptap = ({
               onClick={() => editor.chain().focus().toggleItalic().run()}
               selected={editor.isActive("italic") ? true : false}
             >
-              <FormatItalic />
+              <FormatItalic fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           <Tooltip title="Underline">
@@ -119,7 +239,17 @@ const Tiptap = ({
               onClick={() => editor.chain().focus().toggleUnderline().run()}
               selected={editor.isActive("underline") ? true : false}
             >
-              <FormatUnderlined />
+              <FormatUnderlined fontSize="inherit" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title="Strike">
+            <ToggleButton
+              value="strike"
+              aria-label="strike"
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              selected={editor.isActive("strike") ? true : false}
+            >
+              <FormatStrikethrough fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           {/* <ToggleButton value="color" aria-label="color" disabled>
@@ -135,7 +265,7 @@ const Tiptap = ({
               onClick={() => editor.chain().focus().setTextAlign("left")}
               selected={editor.isActive({ textAlign: "left" }) ? true : false}
             >
-              <FormatAlignLeft />
+              <FormatAlignLeft fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           <Tooltip title="Align Center">
@@ -147,7 +277,7 @@ const Tiptap = ({
               }
               selected={editor.isActive({ textAlign: "center" }) ? true : false}
             >
-              <FormatAlignCenter />
+              <FormatAlignCenter fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           <Tooltip title="Align Right">
@@ -157,7 +287,7 @@ const Tiptap = ({
               onClick={() => editor.chain().focus().setTextAlign("right").run()}
               selected={editor.isActive({ textAlign: "right" }) ? true : false}
             >
-              <FormatAlignRight />
+              <FormatAlignRight fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           <Tooltip title="Justify">
@@ -171,7 +301,7 @@ const Tiptap = ({
                 editor.isActive({ textAlign: "justify" }) ? true : false
               }
             >
-              <FormatAlignJustify />
+              <FormatAlignJustify fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
         </ToggleButtonGroup>
@@ -184,7 +314,7 @@ const Tiptap = ({
               onClick={() => editor.chain().focus().toggleBlockquote().run()}
               selected={editor.isActive("blockquote") ? true : false}
             >
-              <FormatQuote />
+              <FormatQuote fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           <Tooltip title="Ordered List">
@@ -194,7 +324,7 @@ const Tiptap = ({
               onClick={() => editor.chain().focus().toggleOrderedList().run()}
               selected={editor.isActive("orderedList") ? true : false}
             >
-              <FormatListNumbered />
+              <FormatListNumbered fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
           <Tooltip title="Bullet List">
@@ -204,18 +334,38 @@ const Tiptap = ({
               onClick={() => editor.chain().focus().toggleBulletList().run()}
               selected={editor.isActive("orderedList") ? true : false}
             >
-              <FormatListBulleted />
+              <FormatListBulleted fontSize="inherit" />
             </ToggleButton>
           </Tooltip>
         </ToggleButtonGroup>
-        <Tooltip title="Insert Paragraph Break">
-          <IconButton
-            size="small"
-            onClick={() => editor.chain().focus().setHardBreak().run()}
-          >
-            <InsertPageBreakRounded fontSize="inherit" />
-          </IconButton>
-        </Tooltip>
+
+        <ToggleButtonGroup aria-label="text style" size="small">
+          <Tooltip title="Insert Paragraph Break">
+            <ToggleButton
+              value="hard-break"
+              aria-label="hard-break"
+              onClick={() => editor.chain().focus().setHardBreak().run()}
+            >
+              <InsertPageBreakRounded fontSize="inherit" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title="Insert Horizontal Rule">
+            <ToggleButton
+              value="horizontal-rule"
+              aria-label="horizontal-rule"
+              onClick={() => editor.chain().focus().setHorizontalRule().run()}
+            >
+              <HorizontalRule fontSize="inherit" />
+            </ToggleButton>
+          </Tooltip>
+        </ToggleButtonGroup>
+        <ToggleButtonGroup aria-label="text style" size="small">
+          <Tooltip title="Insert Image">
+            <ToggleButton value="image" aria-label="image" onClick={addImage}>
+              <InsertPhoto fontSize="inherit" />
+            </ToggleButton>
+          </Tooltip>
+        </ToggleButtonGroup>
       </div>
       <EditorContent editor={editor} />
     </Paper>
